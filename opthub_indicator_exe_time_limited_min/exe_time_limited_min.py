3fe8804ea1bda5c73c57c3f5b82236b0c5cafeb2
TakatoKinoshita <takato@ieee.org>
2023-10-19 01:04:59 +0900

    logging

--- opthub_indicator_exe_time_limited_min/exe_time_limited_min.py
+++ opthub_indicator_exe_time_limited_min/exe_time_limited_min.py
@@ -1,5 +1,9 @@
 import json
 import os
+from logging import getLogger, basicConfig, CRITICAL
+from traceback import format_exc
+
+LOGGER = getLogger(__name__)
 
 
 def limited_min(solution_to_score: str, solutions_scored: str, limit: int) -> float:
@@ -9,7 +13,9 @@ def limited_min(solution_to_score: str, solutions_scored: str, limit: int) -> fl
         raise ValueError(f"limit must be positive (actually {limit})")
 
     now = json.loads(solution_to_score)
+    LOGGER.debug(f"{now = }")
     until = json.loads(solutions_scored)
+    LOGGER.debug(f"{until = }")
 
     y = now["objective"]
     if not until:
@@ -28,14 +34,26 @@ def limited_min(solution_to_score: str, solutions_scored: str, limit: int) -> fl
 
 def main():
     limit = int(os.getenv("EXE_TIME_LIMIT", 8 * 60 * 60))
+    LOGGER.debug(f"EXE_TIME_LIMIT = {limit}")
     solution_to_score = input()
+    LOGGER.debug(f"{solution_to_score = }")
     solutions_scored = input()
+    LOGGER.debug(f"{solutions_scored = }")
     score = limited_min(solution_to_score, solutions_scored, limit)
+    LOGGER.debug(f"{score = }")
+
     print(json.dumps({"score": score}))
+    LOGGER.debug("End")
 
 
 if __name__ == "__main__":
     try:
+        LOGGER.info("Start")
+        log_level = os.getenv("PY_LOG_LEVEL", CRITICAL)
+        basicConfig(level=log_level)
+        LOGGER.info(f"{log_level = }")
         main()
+        LOGGER.info("Successfully finished")
     except Exception as e:
+        LOGGER.error(format_exc())
         print(json.dumps({"score": None, "error": str(e)}))
