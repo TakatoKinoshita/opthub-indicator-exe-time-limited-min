0c54c93cae4c04011f292efef6a56b36cd61d069
TakatoKinoshita <takato@ieee.org>
2023-10-18 20:21:15 +0900

    fix #3

--- tests/test_exe_time_limited_min.py
+++ tests/test_exe_time_limited_min.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any, List
 
 import pytest
 
@@ -12,152 +13,63 @@ def now():
     )
 
 
-def test_limited_min_normal_return_min_1(now):
-    """
-    最小値を返す
-    """
-    expected = 1.0
-
-    limit = 100
-    until = json.dumps(
+@pytest.fixture
+def solution_list(request):
+    lists = [
         [
             {
                 "id": 1,
                 "objective": 2.0,
                 "constraint": None,
                 "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
             },
             {
                 "id": 2,
                 "objective": 3.0,
                 "constraint": None,
                 "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
             },
             {
                 "id": 3,
                 "objective": 1.0,
                 "constraint": None,
                 "info": {"exe_time": 3, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 1.0,
             },
-        ]
-    )
-
-    score = limited_min(now, until, limit)
-    assert score == expected
-
-
-def test_limited_min_normal_return_min_2(now):
-    """
-    最小値を返す
-    """
-    expected = 1.5
-
-    limit = 100
-    until = json.dumps(
+            {"id": 4, "objective": 1.5, "constraint": None, "info": {"exe_time": 1.5, "delays": [0, 0, 0, 0, 0, 0]}},
+        ],
         [
-            {
-                "id": 1,
-                "objective": 2.0,
-                "constraint": None,
-                "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-            {
-                "id": 2,
-                "objective": 3.0,
-                "constraint": None,
-                "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-            {
-                "id": 3,
-                "objective": 4.0,
-                "constraint": None,
-                "info": {"exe_time": 3, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-        ]
-    )
+            {"id": 1, "objective": 2.0, "constraint": None, "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]}},
+            {"id": 2, "objective": 3.0, "constraint": None, "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]}},
+            {"id": 3, "objective": 4.0, "constraint": None, "info": {"exe_time": 3, "delays": [0, 0, 0, 0, 0, 0]}},
+            {"id": 4, "objective": 1.5, "constraint": None, "info": {"exe_time": 1.5, "delays": [0, 0, 0, 0, 0, 0]}},
+        ],
+    ]
 
-    score = limited_min(now, until, limit)
-    assert score == expected
+    return lists[request.param]
 
 
-def test_limited_min_normal_return_limited_min_1(now):
+@pytest.mark.parametrize(
+    "solution_list, limit, expects",
+    [
+        (0, 100, [2.0, 2.0, 1.0, 1.0]),
+        (1, 100, [2.0, 2.0, 2.0, 1.5]),
+        (0, 6, [2.0, 2.0, 2.0, 2.0]),
+        (0, 8, [2.0, 2.0, 2.0, 2.0]),
+    ],
+    indirect=["solution_list"],
+)
+def test_limited_min_normal(solution_list, limit, expects):
     """
     `exe_time`の合計が`limit`以下までの最小値を返す
     """
-    expected = 2.0
-
-    limit = 6
-    until = json.dumps(
-        [
-            {
-                "id": 1,
-                "objective": 2.0,
-                "constraint": None,
-                "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-            {
-                "id": 2,
-                "objective": 3.0,
-                "constraint": None,
-                "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-            {
-                "id": 3,
-                "objective": 1.0,
-                "constraint": None,
-                "info": {"exe_time": 3, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-        ]
-    )
-
-    score = limited_min(now, until, limit)
-    assert score == expected
-
-
-def test_limited_min_normal_return_limited_min_2(now):
-    """
-    `exe_time`の合計が`limit`以下までの最小値を返す
-    """
-    expected = 2.0
-
-    limit = 8
-    until = json.dumps(
-        [
-            {
-                "id": 1,
-                "objective": 2.0,
-                "constraint": None,
-                "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-            {
-                "id": 2,
-                "objective": 3.0,
-                "constraint": None,
-                "info": {"exe_time": 2, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-            {
-                "id": 3,
-                "objective": 4.0,
-                "constraint": None,
-                "info": {"exe_time": 3, "delays": [0, 0, 0, 0, 0, 0]},
-                "score": 2.0,
-            },
-        ]
-    )
-
-    score = limited_min(now, until, limit)
-    assert score == expected
+    until: List[Any] = []
+    for sol, expect in zip(solution_list, expects):
+        now_str = json.dumps(sol)
+        until_str = json.dumps(until)
+        score = limited_min(now_str, until_str, limit)
+        assert score == expect
+        sol["score"] = score
+        until.append(sol)
 
 
 def test_limited_min_error_limit_negative(now):
